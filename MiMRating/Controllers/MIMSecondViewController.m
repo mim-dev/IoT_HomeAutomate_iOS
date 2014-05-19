07ab34b2c1cfb0169d4453a0683d9b58ccc9fae0
Jason George <jason.george@cenobil.com>
2014-05-19 08:46:09 -0400

    Added physical rating controls

--- MiMRating/Controllers/MIMSecondViewController.m
+++ MiMRating/Controllers/MIMSecondViewController.m
@@ -9,8 +9,19 @@
 #import "MIMSecondViewController.h"
 #import "MIMLocationManager.h"
 #import "MIMUserService.h"
+#import "MIMReportingUser.h"
+
+static NSString * const kErrorTitle = @"Error";
+static NSString * const kCancelTitle = @"OK";
+static NSString * const kFailureMessage = @"Unable to report physical rating";
+static NSString * const kSuccessMessage = @"Physical rating successfully reported";
+static NSString * const kLocationMessage = @"Unable to determine your location";
 
 @interface MIMSecondViewController ()
+
+- (void)reportPhysicalRating:(NSInteger)rating;
+- (void)displayAlertWithTitle:(NSString *)aTitle
+					  message:(NSString *)aMessage;
 
 @end
 
@@ -45,6 +56,73 @@
 							  NSLog(@"error");
 						  }];
 	}
+}
+
+- (IBAction)button1_touchUpInside:(id)sender
+{
+	[self reportPhysicalRating:5];
+}
+
+- (IBAction)button2_touchUpInside:(id)sender
+{
+	[self reportPhysicalRating:4];
+}
+
+- (IBAction)button3_touchUpInside:(id)sender
+{
+	[self reportPhysicalRating:3];
+}
+
+- (IBAction)button4_touchUpInside:(id)sender
+{
+	[self reportPhysicalRating:2];
+}
+
+- (IBAction)button5_touchUpInside:(id)sender
+{
+	[self reportPhysicalRating:1];
+}
+
+- (void)reportPhysicalRating:(NSInteger)aRating
+{
+	MIMLocationManager *locationManager = [MIMLocationManager sharedInstance];
+	if (locationManager.location != nil) {
+		
+		MIMReportingUser *user = [MIMReportingUser new];
+		
+		NSError * __autoreleasing error = nil;
+		MIMUserService *service = [MIMUserService sharedInstance];
+		[service postPhysicalWithUsername:user.username
+								 password:user.password
+								 location:locationManager.location
+									 date:[NSDate date]
+								   rating:[NSNumber numberWithInteger:aRating]
+									error:&error
+						  completionBlock:^(NSError *error){
+							  
+							  if (error == nil) {
+								  [self displayAlertWithTitle:nil
+													  message:kSuccessMessage];
+							  } else {
+								  [self displayAlertWithTitle:kErrorTitle
+													  message:kFailureMessage];
+							  }
+						  }];
+	} else {
+		[self displayAlertWithTitle:kErrorTitle
+							message:kLocationMessage];
+	}
+}
+
+- (void)displayAlertWithTitle:(NSString *)aTitle
+					  message:(NSString *)aMessage
+{
+	UIAlertView *alertView = [[UIAlertView alloc] initWithTitle:aTitle
+														message:aMessage
+													   delegate:nil
+											  cancelButtonTitle:kCancelTitle
+											  otherButtonTitles:nil];
+	[alertView show];
 }
 
 @end
