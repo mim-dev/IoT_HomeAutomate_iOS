a71177232c0bb5f2053e3053a82353409f9d145d
Jason George <jason.george@cenobil.com>
2014-05-19 20:49:33 -0400

    Updated location services control to correct issues enabling location service access

--- MiMRating/MIMAppDelegate.m
+++ MiMRating/MIMAppDelegate.m
@@ -22,7 +22,6 @@
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
     // Override point for customization after application launch.
-	[self startLocationUpdates];
     return YES;
 }
 
@@ -36,6 +35,7 @@
 {
 	// Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later.
 	// If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
+	[self stopLocationUpdates];
 }
 
 - (void)applicationWillEnterForeground:(UIApplication *)application
@@ -46,12 +46,12 @@
 - (void)applicationDidBecomeActive:(UIApplication *)application
 {
 	// Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
+	[self startLocationUpdates];
 }
 
 - (void)applicationWillTerminate:(UIApplication *)application
 {
 	// Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
-	[self stopLocationUpdates];
 }
 
 #pragma mark - Location Management
